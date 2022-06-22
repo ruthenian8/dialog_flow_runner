1fa17adf01177b97467fb3f128143f18a05adcbb
milesartemius <shveitsar215@gmail.com>
2022-06-22 20:27:34 +0300

    provider polling/callable support added

--- df_runner/provider.py
+++ df_runner/provider.py
@@ -1,41 +1,111 @@
+import time
 import uuid
 from abc import abstractmethod, ABC
-from typing import Optional
+from typing import Optional, Any
+
+from df_runner import ProviderFunction
 
 
 class AbsProvider(ABC):
+    """
+    Class that represents a provider used for communication between runner and user.
+    """
+
+    def __init__(
+        self,
+        intro: Optional[str] = None
+    ):
+        self.ctx_id: Optional[Any] = None
+        self._intro: Optional[str] = intro
+        self._callback: Optional[ProviderFunction] = None
+
+    @abstractmethod
+    def setup(self, callback: ProviderFunction):
+        """
+        Method invoked when user first interacts with the runner and dialog starts.
+        A good place to generate self.ctx_id - a unique ID of the dialog.
+        May be used for sending self._intro - an introduction message.
+        :callback: - a function that is run every time user provider an input, returns runner answer.
+        """
+        self._callback = callback
+
+
+class PollingProvider(AbsProvider):
+    """
+    Polling provider runs in a loop, constantly asking user for a new input.
+    """
+
+    def __init__(
+        self,
+        intro: Optional[str] = None,
+        timeout: int = 0
+    ):
+        super().__init__(intro)
+        self._timeout = timeout
+
+    @abstractmethod
+    def _request(self) -> str:
+        """
+        Method used for sending user a request for input.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def _respond(self, response: str):
+        """
+        Method used for sending user a response for his last input.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def setup(self, callback: ProviderFunction):
+        """
+        Method, running a request - response cycle in a loop, sleeping for self._timeout seconds after each iteration.
+        """
+        super().setup(callback)
+        while True:
+            request = self._request()
+            response = self._callback(request)
+            self._respond(response)
+            time.sleep(self._timeout)
+
+
+class CallbackProvider(AbsProvider):
+    """
+    Callback provider is waiting for user input and answers once it gets one.
+    """
+
+    @abstractmethod
+    def _on_input(self, user_input: str) -> str:
+        """
+        Method invoked on user input, should run self._callback function (if any).
+        """
+        raise NotImplementedError
+
+
+class CLIProvider(PollingProvider):
+    """
+    Command line provider - the default provider for each runner.
+    """
+
     def __init__(
         self,
         intro: Optional[str] = None,
         prompt_request: str = "request: ",
-        prompt_response: str = "response: ",
+        prompt_response: str = "response: "
     ):
-        self.ctx_id = None
-        self.intro: Optional[str] = intro
-        self.prompt_request: str = prompt_request
-        self.prompt_response: str = prompt_response
+        super().__init__(intro)
+        self._prompt_request: str = prompt_request
+        self._prompt_response: str = prompt_response
 
-    @abstractmethod
-    def init(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def request(self) -> str:
-        raise NotImplementedError
-
-    @abstractmethod
-    def response(self, response: str):
-        raise NotImplementedError
-
-
-class CLIProvider(AbsProvider):
-    def init(self):
+    def setup(self, callback: ProviderFunction):
         self.ctx_id = uuid.uuid4()
-        if self.intro is not None:
-            print(self.intro)
+        if self._intro is not None:
+            print(self._intro)
+        super().setup(callback)
 
-    def request(self) -> str:
-        return input(self.prompt_request)
+    def _request(self) -> str:
+        return input(self._prompt_request)
 
-    def response(self, response: str):
-        print(f"{self.prompt_response}{response}")
+    def _respond(self, response: str):
+        print(f"{self._prompt_response}{response}")
