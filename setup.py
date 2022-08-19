60647c5312e8fb48ab4290efdaa79b3ec2b24317
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2022-08-19 16:48:44 +0300

    feat/pipeline_concept-prerelease: upd version to 0.2.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ test_requirements = parse_requirements("requirements_test.txt")
 
 setup(
     name="df_runner",
-    version="0.1.1",
+    version="0.2.0",
     description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
