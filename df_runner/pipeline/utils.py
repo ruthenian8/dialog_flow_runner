923c00755520092e30f9e0752ff9c36f1ba51ccb
milesartemius <shveitsar215@gmail.com>
2022-09-06 11:08:24 +0300

    smart collision resolving fixed

--- df_runner/pipeline/utils.py
+++ df_runner/pipeline/utils.py
@@ -1,5 +1,5 @@
 import collections
-from typing import Union, List, Callable, Optional
+from typing import Union, List, Callable
 
 from df_engine.core import Actor
 
@@ -51,7 +51,7 @@ def pretty_format_component_info_dict(
 
 
 def rename_component_incrementing(
-    service: Union[Service, ServiceGroup], collisions: Optional[List[Union[Service, ServiceGroup]]] = None
+    service: Union[Service, ServiceGroup], collisions: List[Union[Service, ServiceGroup]]
 ):
     """
     Function for generating new name for a pipeline component,
@@ -64,7 +64,7 @@ def rename_component_incrementing(
         5. After that, '_[NUMBER]' is added to the resulting name,
             where number is number of components with the same name in current service group.
             NB! This happens only if `collisions` argument is not None.
-    :service: (required) - service to be renamed.
+    :service: - service to be renamed.
     :collisions: - services in the same service group as service.
     Returns string - generated name.
     """
@@ -76,11 +76,13 @@ def rename_component_incrementing(
         base_name = "service_group"
     else:
         base_name = "noname_service"
-    if collisions is None:
+
+    collision_names = [component.name for component in collisions]
+    if base_name not in collision_names:
         return base_name
     else:
         name_index = 0
-        while f"{base_name}_{name_index}" in [serv.name for serv in collisions]:
+        while f"{base_name}_{name_index}" in collision_names:
             name_index += 1
         return f"{base_name}_{name_index}"
 
@@ -97,13 +99,11 @@ def finalize_service_group(service_group: ServiceGroup, path: str = ".") -> Acto
     actor = None
     names_counter = collections.Counter([component.name for component in service_group.services])
     for component in service_group.services:
-        if component.name is None:
-            if names_counter[component.name] > 1:
+        if names_counter[component.name] > 1:
+            if component.name is None:
                 component.name = rename_component_incrementing(component, service_group.services)
-            elif names_counter[component.name] == 1:
-                component.name = rename_component_incrementing(component, None)
-        elif names_counter[component.name] > 1:
-            raise Exception(f"User defined service name collision ({path})!")
+            else:
+                raise Exception(f"User defined service name collision ({path})!")
         component.path = f"{path}.{component.name}"
 
         if isinstance(component, Service) and isinstance(component.handler, Actor):
